5cee34f9c0ea04c48df4fd39178b8cd9534489e2
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-15 21:40:28 +0700

    add recommendation model

---
+++ src/model/recommendation-model.ts
@@ -0,0 +1,13 @@
+export type MenuRecommendationResponse = {
+    id: number;
+    rank: number;
+    description: string;
+}
+
+export type MenuRecommendationRequest = {
+
+}
+
+export type GetMenuRecommendationRequest = {
+
+}
