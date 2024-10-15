497897a0cac28739e150c830712dab1dae6daa8d
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-15 21:51:35 +0700

    add menu controller

---
+++ src/controller/menu-controller.ts
@@ -0,0 +1,62 @@
+import {Request, Response, NextFunction} from "express";
+import {CreateMenuNutritionRequest, CreateMenuRequest, DeleteMenuRequest, UpdateMenuRequest} from "../model/menu-model";
+import {MenuService} from "../service/menu-service";
+import {logger} from "../application/logging";
+
+export class MenuController {
+    static async createMenu(req: Request, res: Response, next: NextFunction) {
+        try {
+            const request: CreateMenuRequest = req.body as CreateMenuRequest;
+            const response = await MenuService.createMenu(request);
+            res.status(201).json({
+                message: "Menu berhasil ditambahkan, notifikasi terkirim kepada admin",
+                data: response
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static async updateMenu(req: Request, res: Response, next: NextFunction) {
+        try {
+            const request: UpdateMenuRequest = req.body as UpdateMenuRequest;
+            request.menu_id = Number(req.params.menuId);
+            const response = await MenuService.updateMenu(request);
+            res.status(201).json({
+                message: "Menu berhasil ditambahkan, notifikasi terkirim kepada admin",
+                data: response
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static async deleteMenu(req: Request, res: Response, next: NextFunction) {
+        try {
+            const request: DeleteMenuRequest = req.body as DeleteMenuRequest;
+            request.menu_id = Number(req.params.menuId);
+            const response = await MenuService.deleteMenu(request);
+            res.status(200).json({
+                message: "Menu berhasil dihapus",
+            })
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    static async createMenuNutrition(req: Request, res: Response, next: NextFunction) {
+        try {
+            const request: CreateMenuNutritionRequest = req.body as CreateMenuNutritionRequest;
+            request.menu_id = Number(req.params.menuId);
+            const response = await MenuService.createMenuNutrition(request);
+            res.status(201).json({
+                message: "Nutrisi menu berhasil ditambahkan",
+                data: response
+            })
+        } catch (e) {
+            next(e);
+        }
+    }
+
+
+}
