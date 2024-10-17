a7324ce1d50509a4907b79bdd991f1d4fd780347
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-17 16:11:51 +0700

    updating route

--- src/route/private-api.ts
+++ src/route/private-api.ts
@@ -1,13 +1,25 @@
 import express from "express";
 import {UserController} from "../controller/user-controller";
-import {authorizeMiddleware, Roles} from "../middleware/auth-middleware";
+import {authorizeMiddleware} from "../middleware/auth-middleware";
 import {MenuController} from "../controller/menu-controller";
+import {OrderController} from "../controller/order-controller";
+import {NotificationController} from "../controller/notification-controller";
+import {Roles} from "../type/role";
 
 export const privateRouter = express.Router();
 
 
-// Consumer
-// privateRouter.get("/api/users/consumer/profile", authorizeMiddleware(Roles.Consumer), UserController.getProfile);
+// User Module
+// @ts-ignore
+privateRouter.get("/api/users/consumer/profile", authorizeMiddleware(Roles.Consumer), UserController.getProfileConsumer);
+// @ts-ignore
+privateRouter.get("/api/users/consumer/:consumerId([a-zA-Z0-9_-]+)", authorizeMiddleware(Roles.Admin), UserController.getProfileConsumer);
+// @ts-ignore
+privateRouter.get("api/users/restaurant/profile", authorizeMiddleware(Roles.Restaurant), UserController.getProfileRestaurant)
+// @ts-ignore
+privateRouter.get("/api/users/restaurant/:restaurantId([a-zA-Z0-9_-]+)", authorizeMiddleware(Roles.AdminAndConsumer), UserController.getProfileRestaurant)
+
+
 
 
 // Menu Module
@@ -17,3 +29,12 @@ privateRouter.delete("/api/menu/:menuId(\\d+)", MenuController.deleteMenu);
 privateRouter.post("/api/menu/:menuId(\\d+)/nutrition", MenuController.createMenuNutrition);
 privateRouter.put("/api/menu/:menuId(\\d+)/nutrition", MenuController.updateMenuNutrition);
 privateRouter.patch("/api/menu/:menuId(\\d+)/status", MenuController.updateMenuApproval);
+
+// Order Module
+privateRouter.post("/api/order", OrderController.createOrder);
+
+// Notification Module
+// @ts-ignore
+privateRouter.get("/api/notifications", NotificationController.getNotification);
+// @ts-ignore
+privateRouter.patch("/api/notifications/:notificationId(\\d+)", NotificationController.updateNotificationRead);
--- src/route/public-api.ts
+++ src/route/public-api.ts
@@ -5,4 +5,5 @@ import {AuthController} from "../controller/auth-controller";
 export const publicRouter = express.Router();
 publicRouter.post("/api/signup/consumer", UserController.registerConsumer);
 publicRouter.post("/api/signup/restaurant", UserController.registerRestaurant);
+publicRouter.post("/api/signup/admin", UserController.registerAdmin);
 publicRouter.post("/api/auth/login", AuthController.login);
