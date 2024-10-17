5d06822dc003647b62f330c584789565573b532f
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-17 16:12:45 +0700

    add role type

---
+++ src/type/role.ts
@@ -0,0 +1,8 @@
+export const Roles = {
+    Consumer: ["Konsumen"],
+    Restaurant: ["Restoran"],
+    Admin: ["Admin"],
+    AdminAndConsumer: ["Admin", "Konsumen"],
+    AdminAndRestaurant: ["Admin", "Restoran"],
+    All: ["Konsumen", "Restoran", "Admin"],
+};
