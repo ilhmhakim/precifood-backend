1bf0d040e53ad23306eae2f48438a0b941785837
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-17 16:11:17 +0700

    add notification model

--- src/model/notification-model.ts
+++ src/model/notification-model.ts
@@ -1,3 +1,5 @@
+import {Notification} from "@prisma/client";
+
 export type NotificationResponse = {
     id: number;
     title: string;
@@ -15,4 +17,22 @@ export type CreateNotificationRequest = {
     menu_id: number;
     menu_name: string;
     is_read: boolean;
+}
+
+export type UpdateNotificationReadRequest = {
+    id: number;
+    is_read: boolean;
+}
+
+
+export function toNotificationResponse(notification: Notification): NotificationResponse {
+    return {
+        id: notification.id,
+        title: notification.title,
+        restaurant_name: notification.restaurant_name,
+        restaurant_id: notification.restaurant_id,
+        menu_id: notification.menu_id,
+        menu_name: notification.menu_name,
+        is_read: notification.is_read
+    }
 }
