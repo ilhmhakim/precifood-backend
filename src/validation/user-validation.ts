4a41f0fccf1882dd6f5f6ad5dc42c2d3e2e3259e
Ilham Hakim <ilhmhakim@gmail.com>
2024-10-17 16:15:26 +0700

    update user and order validation

--- src/validation/user-validation.ts
+++ src/validation/user-validation.ts
@@ -27,6 +27,15 @@ export class UserValidation {
         image: z.string().min(1),
         password: z.string().min(8),
         password_confirmation: z.string().min(8)
+    });
 
+    static readonly REGISTERADMIN: ZodType = z.object({
+        email: z.string().email(),
+        password: z.string().min(8),
+        password_confirmation: z.string().min(8)
+    });
+
+    static readonly GETUSERPROFILE: ZodType = z.object({
+        id: z.string().min(38).max(38)
     });
 }
