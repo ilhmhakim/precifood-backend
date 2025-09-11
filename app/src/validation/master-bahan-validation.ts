ebb07062d7293607853073e852a647778ce9364b
AghnatHs <aghnathasya@gmail.com>
2025-09-11 20:48:19 +0700

    Change MasterBahan and MasterBumbu nutrition validation to max 20 decimal places

--- app/src/validation/master-bahan-validation.ts
+++ app/src/validation/master-bahan-validation.ts
@@ -20,62 +20,63 @@ export class MasterBahanValidation {
     calory: z
       .number({ required_error: 'Kalori harus diisi' })
       .min(0, 'Kalori tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Kalori harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Kalori harus memiliki maksimal 20 angka di belakang koma',
       }),
     protein: z
       .number({ required_error: 'Protein harus diisi' })
       .min(0, 'Protein tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Protein harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Protein harus memiliki maksimal 20 angka di belakang koma',
       }),
     fat: z
       .number({ required_error: 'Lemak harus diisi' })
       .min(0, 'Lemak tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Lemak harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Lemak harus memiliki maksimal 20 angka di belakang koma',
       }),
     carbohydrate: z
       .number({ required_error: 'Karbohidrat harus diisi' })
       .min(0, 'Karbohidrat tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Karbohidrat harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message:
+          'Karbohidrat harus memiliki maksimal 20 angka di belakang koma',
       }),
     fiber: z
       .number({ required_error: 'Serat harus diisi' })
       .min(0, 'Serat tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Serat harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Serat harus memiliki maksimal 20 angka di belakang koma',
       }),
     natrium: z
       .number({ required_error: 'Natrium harus diisi' })
       .min(0, 'Natrium tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Natrium harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Natrium harus memiliki maksimal 20 angka di belakang koma',
       }),
     cholesterol: z
       .number({ required_error: 'Kolesterol harus diisi' })
       .min(0, 'Kolesterol tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Kolesterol harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Kolesterol harus memiliki maksimal 20 angka di belakang koma',
       }),
     sfa: z
       .number({ required_error: 'SFA harus diisi' })
       .min(0, 'SFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'SFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'SFA harus memiliki maksimal 20 angka di belakang koma',
       }),
     mufa: z
       .number({ required_error: 'MUFA harus diisi' })
       .min(0, 'MUFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'MUFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'MUFA harus memiliki maksimal 20 angka di belakang koma',
       }),
     pufa: z
       .number({ required_error: 'PUFA harus diisi' })
       .min(0, 'PUFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'PUFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'PUFA harus memiliki maksimal 20 angka di belakang koma',
       }),
   });
 
@@ -101,71 +102,72 @@ export class MasterBahanValidation {
     calory: z
       .number()
       .min(0, 'Kalori tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Kalori harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Kalori harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     protein: z
       .number()
       .min(0, 'Protein tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Protein harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Protein harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     fat: z
       .number()
       .min(0, 'Lemak tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Lemak harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Lemak harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     carbohydrate: z
       .number()
       .min(0, 'Karbohidrat tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Karbohidrat harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message:
+          'Karbohidrat harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     fiber: z
       .number()
       .min(0, 'Serat tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Serat harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Serat harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     natrium: z
       .number()
       .min(0, 'Natrium tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Natrium harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Natrium harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     cholesterol: z
       .number()
       .min(0, 'Kolesterol tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'Kolesterol harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'Kolesterol harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     sfa: z
       .number()
       .min(0, 'SFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'SFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'SFA harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     mufa: z
       .number()
       .min(0, 'MUFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'MUFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'MUFA harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
     pufa: z
       .number()
       .min(0, 'PUFA tidak boleh negatif')
-      .refine((val) => /^\d+(\.\d{1,2})?$/.test(val.toString()), {
-        message: 'PUFA harus memiliki maksimal 2 angka di belakang koma',
+      .refine((val) => /^\d+(\.\d{1,20})?$/.test(val.toString()), {
+        message: 'PUFA harus memiliki maksimal 20 angka di belakang koma',
       })
       .optional(),
   });
